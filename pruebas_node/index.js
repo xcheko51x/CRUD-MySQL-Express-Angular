84890c245a627941ae34bb63b5bc3341130f56bd
xcheko51x <xcheko51x@gmail.com>
2023-01-20 12:15:52 -0600

    Update index.js

--- pruebas_node/index.js
+++ pruebas_node/index.js
@@ -7,6 +7,7 @@ const app = express()
 app.use(function(req, res, next) {
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Methods', '*');
+    res.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next()
 })
 
